3e382052eab04877dadba7be02b6fd719bdaf31d
karolk98 <47919588+karolk98@users.noreply.github.com>
2020-10-06 18:09:04 +0200

    Basic WinForms application with OpenGL context

---
+++ RayTracer/MainForm.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenTK.Graphics.ES30;
+using RayTracer.Utils;
+
+namespace RayTracer
+{
+    public partial class MainForm : Form
+    {
+        private bool _loaded;
+        private Shader shader;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        
+        private void GLControl_Load(object sender, EventArgs e)
+        {
+            GL.ClearColor(0.3f, 0.2f, 0.3f, 1.0f);
+            GL.Enable(EnableCap.DepthTest);
+            GL.Viewport(0, 0, gLControl.Width, gLControl.Height);
+            shader = new Shader("shader.vert", "shader.frag");
+            shader.Use();
+            _loaded = true;
+            gLControl.Invalidate();
+        }
+
+        private void GLControl_Resize(object sender, EventArgs e)
+        {
+            if (!_loaded)
+                return;
+            GL.Viewport(0, 0, gLControl.Width, gLControl.Height);
+            gLControl.Invalidate();
+        }
+
+        private void GLControl_Paint(object sender, PaintEventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            shader.Use();
+            gLControl.SwapBuffers();
+        }
+    }
+}
---
+++ RayTracer/Utils/Shader.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using OpenTK.Graphics.ES30;
+
+namespace RayTracer.Utils
+{
+    class Shader
+    {
+        int Handle;
+
+        public Shader(string vertexPath, string fragmentPath)
+        {
+            int VertexShader;
+            int FragmentShader;
+
+            string VertexShaderSource;
+            var assembly = typeof(Program).Assembly;
+            Stream vertexShaderStream = assembly.GetManifestResourceStream("RayTracer.Shaders."+vertexPath);
+            Stream fragmentShaderStream = assembly.GetManifestResourceStream("RayTracer.Shaders."+fragmentPath);
+
+            using (StreamReader reader = new StreamReader(vertexShaderStream, Encoding.UTF8))
+            {
+                VertexShaderSource = reader.ReadToEnd();
+            }
+
+            string FragmentShaderSource;
+
+            using (StreamReader reader = new StreamReader(fragmentShaderStream, Encoding.UTF8))
+            {
+                FragmentShaderSource = reader.ReadToEnd();
+            }
+
+            VertexShader = GL.CreateShader(ShaderType.VertexShader);
+            GL.ShaderSource(VertexShader, VertexShaderSource);
+
+            FragmentShader = GL.CreateShader(ShaderType.FragmentShader);
+            GL.ShaderSource(FragmentShader, FragmentShaderSource);
+
+            GL.CompileShader(VertexShader);
+
+            string infoLogVert = GL.GetShaderInfoLog(VertexShader);
+            if (infoLogVert != System.String.Empty)
+                Console.WriteLine(infoLogVert);
+
+            GL.CompileShader(FragmentShader);
+
+            string infoLogFrag = GL.GetShaderInfoLog(FragmentShader);
+
+            if (infoLogFrag != System.String.Empty)
+                Console.WriteLine(infoLogFrag);
+
+            Handle = GL.CreateProgram();
+
+            GL.AttachShader(Handle, VertexShader);
+            GL.AttachShader(Handle, FragmentShader);
+
+            GL.LinkProgram(Handle);
+
+            GL.DetachShader(Handle, VertexShader);
+            GL.DetachShader(Handle, FragmentShader);
+            GL.DeleteShader(FragmentShader);
+            GL.DeleteShader(VertexShader);
+        }
+
+        public void Use()
+        {
+            GL.UseProgram(Handle);
+        }
+
+        private bool disposedValue = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                GL.DeleteProgram(Handle);
+
+                disposedValue = true;
+            }
+        }
+
+        ~Shader()
+        {
+            GL.DeleteProgram(Handle);
+        }
+
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
