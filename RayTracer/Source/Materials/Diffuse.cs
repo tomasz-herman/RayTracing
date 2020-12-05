4f6ad4ae2a8c034a20b043e73a74121d831bbcc9
Tomasz Herman <tomasz.herman@protonmail.com>
2020-12-05 19:36:35 +0100

    Abstract sampler parameter

--- RayTracer/Source/Materials/Diffuse.cs
+++ RayTracer/Source/Materials/Diffuse.cs
@@ -8,9 +8,9 @@ namespace RayTracing.Materials
     public class Diffuse : IMaterial
     {
         public Color Albedo;
-        private Sampler<Vector3> sampler;
+        private AbstractSampler<Vector3> sampler;
 
-        public Diffuse(Color albedo, Sampler<Vector3> sampler = null)
+        public Diffuse(Color albedo, AbstractSampler<Vector3> sampler = null)
         {
             this.sampler = sampler ?? new Sampler<Vector3>(Vec3Sampling.UniformSphere, 125, 8);
             Albedo = albedo;
