bdcd07fa74cc60accc7470b123d447719374b8b4
karolk98 <47919588+karolk98@users.noreply.github.com>
2020-12-05 21:12:13 +0100

    Added plane model

---
+++ RayTracer/Source/Models/Plane.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using OpenTK;
+using RayTracing.Maths;
+
+namespace RayTracing.Models
+{
+    public class Plane : Model
+    {
+        private Vector3 _normal = Vector3.UnitY;
+        private Vector3 _rotation;
+
+        public override Vector3 Rotation
+        {
+            get => _rotation;
+            set
+            {
+                _rotation = value;
+                _normal= Vector3.UnitY;
+                _normal *= Matrix3.CreateRotationX(_rotation.X);
+                _normal *= Matrix3.CreateRotationY(_rotation.Y);
+                _normal *= Matrix3.CreateRotationZ(_rotation.Z);
+            }
+        }
+
+        private protected override void LoadInternal()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override bool HitTest(Ray ray, ref HitInfo hit, float from, float to)
+        {
+            float t = Vector3.Dot(Position - ray.Origin, _normal) / Vector3.Dot(ray.Direction, _normal);
+            if (t > from && t < to)
+            {
+                hit.Distance = t;
+                hit.HitPoint = ray.Origin + ray.Direction * hit.Distance;
+                hit.ModelHit = this;
+                hit.Normal = _normal;
+                return true;
+            }
+
+            return false;
+        }
+
+        public override Mesh GetMesh()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
