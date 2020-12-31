f8dd853b02648b4646159f4d6730a110156667b8
Tomasz Herman <tomasz.herman@protonmail.com>
2020-12-31 16:28:48 +0000

    Master material alternative implementation

--- RayTracer/Source/Materials/MasterMaterial.cs
+++ RayTracer/Source/Materials/MasterMaterial.cs
@@ -1,72 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using RayTracing.Maths;
+﻿using RayTracing.Maths;
 using RayTracing.Sampling;
 
 namespace RayTracing.Materials
 {
     public class MasterMaterial : IMaterial
     {
-        public Emissive Emissive { get; set; }
-        public Diffuse Diffuse { get; set; }
-        public Reflective Reflective { get; set; }
-        public Refractive Refractive { get; set; }
-        public int EmissivePart { get; set; }
-        public int DiffusePart { get; set; }
-        public int ReflectivePart { get; set; }
-        public int RefractivePart { get; set; }
-        private readonly AbstractSampler<float> _sampler;
-
-        public MasterMaterial(AbstractSampler<float> sampler = null)
-        {
-            _sampler = sampler ?? new ThreadSafeSampler<float>(FloatSampling.Random, 10000);
-        }
+        private const int SAMPLES = 10000;
         
-        private IMaterial GetMaterial()
+        private IMaterial[] Materials;
+        public Emissive Emissive => (Emissive) Materials[1];
+        public Diffuse Diffuse => (Diffuse) Materials[2];
+        public Reflective Reflective => (Reflective) Materials[3];
+        public Refractive Refractive => (Refractive) Materials[4];
+
+        public (float emissive, float diffuse, float reflective, float refractive) Parts
         {
-            var probabilities = new List<int>(4);
-            var materials = new List<IMaterial>(4);
-            if (Emissive != null)
+            get => parts;
+            set
             {
-                probabilities.Add(EmissivePart);
-                materials.Add(Emissive);
+                parts = value;
+                _sampler = NewSampler();
             }
-            if (Diffuse != null)
-            {
-                probabilities.Add(DiffusePart);
-                materials.Add(Diffuse);
-            }
-            if (Reflective != null)
-            {
-                probabilities.Add(ReflectivePart);
-                materials.Add(Reflective);
-            }
-            if (Refractive != null)
-            {
-                probabilities.Add(RefractivePart);
-                materials.Add(Refractive);
-            }
-            
-            float rand = _sampler.GetSample();
-            int sum = probabilities.Sum();
-            int seek = (int) (rand * sum) + 1;
-            int sought = 0;
-            for (int i = 0; i < probabilities.Count; i++)
-            {
-                if (sought < seek && seek <= sought + probabilities[i])
-                    return materials[i];
+        }
 
-                sought += probabilities[i];
-            }
+        private AbstractSampler<int> _sampler;
+        private (float emissive, float diffuse, float reflective, float refractive) parts;
 
-            throw new Exception("material not found");
+        public MasterMaterial(AbstractSampler<int> sampler = null)
+        {
+            Materials = new IMaterial[]
+            {
+                new Emissive(new Color()), //DummyColor, used when all probabilities are 0, ~black body
+                new Emissive(new Color()),
+                new Diffuse(new Color()),
+                new Reflective(new Color()),
+                new Refractive(new Color(), 1)
+            };
+            _sampler = sampler ?? NewSampler();
+        }
+
+        private ThreadSafeSampler<int> NewSampler()
+        {
+            return new ThreadSafeSampler<int>(count => IntSampling.Distribution(count, 
+                parts.emissive, 
+                parts.diffuse, 
+                parts.reflective,
+                parts.refractive), SAMPLES);
         }
 
         public bool Scatter(ref Ray ray, ref HitInfo hit, out Color attenuation, out Ray scattered)
         {
-            var material = GetMaterial();
-            return material.Scatter(ref ray, ref hit, out attenuation, out scattered);
+            return Materials[_sampler.Sample].Scatter(ref ray, ref hit, out attenuation, out scattered);
         }
     }
 }
